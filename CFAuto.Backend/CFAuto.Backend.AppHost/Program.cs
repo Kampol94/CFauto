dab2cf4132bc1715c15ca3a28f3d08567f0b62be
Kamil <kamil.polaczek.dev@gmail.com>
2025-03-29 17:37:30 +0100

    refactor Program.cs to streamline project initialization and remove unused cache configuration

--- CFAuto.Backend/CFAuto.Backend.AppHost/Program.cs
+++ CFAuto.Backend/CFAuto.Backend.AppHost/Program.cs
@@ -1,9 +1,6 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
 
-var cache = builder.AddRedis("redis")
-                    .WithRedisCommander();
-
 var rabbitmq = builder.AddRabbitMQ("rabbitmq")
                       .WithLifetime(ContainerLifetime.Persistent)
                       .WithManagementPlugin();
@@ -13,11 +10,11 @@ var postgres = builder.AddPostgres("postgres")
 
 var postgresdb = postgres.AddDatabase("postgresdb");
 
-var reservationProccessor = builder.AddProject<Projects.ReservationProcessor>("reservationprocessor")
+builder.AddProject<Projects.ReservationProcessor>("reservationprocessor")
     .WithReference(rabbitmq)
     .WaitFor(rabbitmq);
 
-var reservationSummery = builder.AddProject<Projects.ReservationSummery>("reservationsummery")
+builder.AddProject<Projects.ReservationSummery>("reservationsummery")
     .WithReference(postgresdb)
     .WaitFor(postgresdb)
     .WithReference(rabbitmq)
@@ -28,12 +25,5 @@ builder.AddProject<Projects.Web>("web")
     .WithExternalHttpEndpoints()
     .WithReference(rabbitmq)
     .WaitFor(rabbitmq);
-
-// builder.AddProject<Projects.CFAuto_Backend_Web>("webfrontend")
-//     .WithExternalHttpEndpoints()
-//     .WithReference(cache)
-//     .WaitFor(cache)
-//     .WithReference(apiService)
-//     .WaitFor(apiService);
 
 builder.Build().Run();
